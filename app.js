fc8c7da870faf2c9a48568e249496222afc03d47
Henk Burgstra <henk.burgstra@gmail.com>
2016-03-04 09:14:27 +0100

    werk aan composability.js

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var b = new BoxPanel("id", "test");
+    b.setTemplate();
+    b.render();
+});
--- composability.js
+++ composability.js
@@ -1,12 +1,22 @@
-var view = function(parent, name) {
+var View = function(parent, name) {
     this.parent = parent;
     this.name = name;
     this.template = null;
 
+    this.add = function(template) {
+    };
+    this.render = function() {
+        for (var i = 0; i < this.template.items.length; i++) {
+            this.add(this.template.items[i]);
+        }
+    };
     this.setTemplate = function(template) {
         this.template = template;
     };
 
-    this.render = function() {
-    }
-}
+};
+
+var BoxPanel = function(parent, name) {
+    this.prototype = new View(parent, name);
+    this.ancestor = View.prototype;
+};
