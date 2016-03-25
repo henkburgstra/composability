c44b671adf986d6cb83c97cd7f8f10a2064461f5
Henk Burgstra <henk.burgstra@gmail.com>
2016-03-25 17:53:40 +0100

    BoxPanel.createWidget()

--- composability.js
+++ composability.js
@@ -215,6 +215,16 @@ BoxPanel.prototype.createDOM = function () {
 BoxPanel.prototype.createPanels = function() {
 };
 
+BoxPanel.prototype.createWidget = function(parent, template, withLabel) {
+    var widgets = this.ancestor.createWidget.call(this, parent, template, withLabel);
+    if (template.kind == VK.TEMPLATE) {
+        var box = BoxPanel(this.element, template.name);
+        box.setTemplate(template);
+        widgets[1] = box;
+    }
+    return widgets;
+}
+
 BoxPanel.prototype.addContainer = function(parentView, template) {
     this.ancestor.addContainer.call(this, parentView, template);
     if (template.display == Template.DISP_INLINE) {
