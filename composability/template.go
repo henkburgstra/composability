ed9b9ec65202c6517731f8cb2ce840f4916edf2a
Henk Burgstra <henk.burgstra@gmail.com>
2016-04-04 18:25:35 +0200

    werk aan composability.go

--- composability/template.go
+++ composability/template.go
@@ -1,4 +1,21 @@
 package composability
 
 type Template struct {
+	Kind       string      `json:"kind"`
+	Name       string      `json:"name"`
+	Title      string      `json:"title"`
+	Value      interface{} `json:"value"`
+	Readonly   bool
+	Visible    bool
+	Items      []*Template
+	items_map  map[string]*Template
+	Attributes map[string]interface{} `json:"attributes"`
+}
+
+func NewTemplate(kind string) *Template {
+	t := new(Template)
+	t.Kind = kind
+	t.Items = make([]*Template, 0, 0)
+	t.items_map = make(map[string]*Template)
+	return t
 }
