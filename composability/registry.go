d35d37b6606b6f33d134f7654d17c3f852b97fdb
Henk Burgstra <henk.burgstra@gmail.com>
2016-04-07 08:35:52 +0200

    werk aan registry.go

--- composability/registry.go
+++ composability/registry.go
@@ -1,5 +1,12 @@
 package composability
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path"
+)
+
 type Registry struct {
 	defPath   string
 	templates map[string]*Template
@@ -10,4 +17,25 @@ func NewRegistry(defPath string) *Registry {
 	r.defPath = defPath
 	r.templates = make(map[string]*Template)
 	return r
+}
+
+func (r *Registry) LoadDefItem(def *Template) *Template {
+	return def
+}
+
+func ReadError(filename string) {
+	panic(fmt.Sprintf("Can't open template %s", filename))
+}
+func (r *Registry) LoadTemplate(name string) *Template {
+	filename := path.Join(r.defPath, name)
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		ReadError(filename)
+	}
+	t := new(Template)
+	err = json.Unmarshal(data, t)
+	if err != nil {
+		ReadError(filename)
+	}
+	return r.LoadDefItem(t)
 }
