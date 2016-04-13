9a35b57eb479bd88880732c7c00e0e1c11cac2e0
Henk Burgstra <henk.burgstra@gmail.com>
2016-04-13 21:41:27 +0200

    Geen constante path in registry_test.go

--- composability/registry_test.go
+++ composability/registry_test.go
@@ -2,11 +2,17 @@ package composability
 
 import (
 	"fmt"
+	"os"
+	"path"
 	"testing"
 )
 
 func TestLoadTemplate(t *testing.T) {
-	r := NewRegistry(`d:\projecten\python\composability`)
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Error("TestLoadTemplate: os.Getwd() failes")
+	}
+	r := NewRegistry(path.Dir(wd))
 	def := r.LoadTemplate("patient.json")
 	fmt.Println(def.Name)
 }
