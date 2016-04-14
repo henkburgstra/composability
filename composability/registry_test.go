87c0cf196f7fbe9fa870e0485f24583b4fd94483
Henk Burgstra <henk.burgstra@gmail.com>
2016-04-14 08:22:06 +0200

    path.Dir werkt altijd met slashes, filepath.Dir is platform onafhankelijk.

--- composability/registry_test.go
+++ composability/registry_test.go
@@ -3,16 +3,16 @@ package composability
 import (
 	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 	"testing"
 )
 
 func TestLoadTemplate(t *testing.T) {
 	wd, err := os.Getwd()
 	if err != nil {
-		t.Error("TestLoadTemplate: os.Getwd() failes")
+		t.Error("TestLoadTemplate: os.Getwd() failed")
 	}
-	r := NewRegistry(path.Dir(wd))
+	r := NewRegistry(filepath.Dir(wd))
 	def := r.LoadTemplate("patient.json")
 	fmt.Println(def.Name)
 }
