c1c07507790e178719c5e23dffd8a63e5f9d98e1
Henk Burgstra <henk.burgstra@gmail.com>
2016-04-28 08:59:10 +0200

    unittests

--- composability/transformers_test.go
+++ composability/transformers_test.go
@@ -15,19 +15,25 @@ func TestTransformDate(t *testing.T) {
 }
 
 func TestTransformDateDisplay(t *testing.T) {
-	d := "22-08-1965"
-	x := NewTransformDate(d)
-	d2 := x.Display()
-	if d != d2 {
-		t.Errorf("Display is '%s', expected '%s'", d2, d)
+	in := []string{"22-08-1965", "1965-08-22"}
+	expected := in[0]
+	for _, d := range in {
+		x := NewTransformDate(d)
+		d2 := x.Display()
+		if expected != d2 {
+			t.Errorf("Display is '%s', expected '%s'", d2, expected)
+		}
 	}
 }
 
 func TestTransformDateStore(t *testing.T) {
-	d := "1965-08-22"
-	x := NewTransformDate("22-08-1965")
-	d2 := x.Store()
-	if d != d2 {
-		t.Errorf("Display is '%s', expected '%s'", d2, d)
+	in := []string{"22-08-1965", "1965-08-22"}
+	expected := in[1]
+	for _, d := range in {
+		x := NewTransformDate(d)
+		d2 := x.Store()
+		if expected != d2 {
+			t.Errorf("Display is '%s', expected '%s'", d2, expected)
+		}
 	}
 }
